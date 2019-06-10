9a1ff4c0cd209e12f551d7a4fc5e87d16b5d8027
Alex Johnson <contact@alexfjohnson.com>
2019-06-09 19:59:25 -0700

    add about lorem

--- src/components/views/About.js
+++ src/components/views/About.js
@@ -5,7 +5,31 @@ const About = () => {
   return (
     <>
       <ContentBackground />
-      <div className="content__container">About</div>
+      <div className="content__container">
+        <p>
+          He meant to write you, but was delayed. Meanwhile, he sent me most of
+          the magazines with your articles, and I saw at once, from your
+          drawings and descriptions, that my stones are certainly the kind you
+          mean. You can appreciate this from the enclosed prints. Later on you
+          will hear directly from Dr. Boyle. Now I can understand how important
+          all this will be to you. Without question we are faced with the
+          remains of an unknown civilization older than any dreamed of before,
+          and forming a basis for your legends. As a mining engineer, I have
+          some knowledge of geology, and can tell you that these blocks are so
+          ancient they frighten me. They are mostly sandstone and granite,
+          though one is almost certainly made of a queer sort of cement or
+          concrete. They bear evidence of water action, as if this part of the
+          world had been submerged and come up again after long ages—all since
+          those blocks were made and used. It is a matter of hundreds of
+          thousands of years—or heaven knows how much more. I don't like to
+          think about it. In view of your previous diligent work in tracking
+          down the legends and everything connected with them, I cannot doubt
+          but that you will want to lead an expedition to the desert and make
+          some archaeological excavations. Both Dr. Boyle and I are prepared to
+          cooperate in such work if you—or organizations known to you—can
+          furnish the funds.
+        </p>
+      </div>
     </>
   );
 };
